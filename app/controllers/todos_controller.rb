74b5b03e0a4955804f614b20a29c29d716631d9f
Apprentice <apprentice@devbootcamp.com>
2013-06-12 11:41:11 -0700

    removed load_todos method on todo controller, killed the before filter

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  before_filter :load_todos
+  
 
   def index
     @todos = Todo.all
@@ -29,18 +29,12 @@ class TodosController < ApplicationController
 
   def update
     @todo = Todo.find params[:id]
-    list_name = params[:todo][:list_name].parameterize
     if @todo.update_attributes params[:todo]
-      @todo.update_attributes :list_name => list_name
       redirect_to @todo
     else
       render :edit
     end
   end
 
-  private
 
-  def load_todos
-    @todos = Todo.all
-  end
 end
